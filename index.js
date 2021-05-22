89572ed5b29c6612e838433056f90f6ee08c655f
Shu Gen <gen_shu@caddi.jp>
2021-05-22 15:06:26 +0900

    add custom scalar DateTime

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { readFileSync } = require('fs')
 const { ApolloServer } = require('apollo-server')
+const { GraphQLScalarType } = require('graphql')
 
 var typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 
@@ -15,20 +16,23 @@ var photos = [
     "name": "Dropping the Heart Chute",
     "description": "The heart chute is one of my favorite chutes",
     "category": "ACTION",
-    "githubUser": "gPlake"
+    "githubUser": "gPlake",
+    "created": "3-28-1977"
   },
   {
     "id": "2",
     "name": "Enjoying the sunshine",
     "category": "SELFIE",
-    "githubUser": "sSchmidt"
+    "githubUser": "sSchmidt",
+    "created": "1-2-1985"
   },
   {
     "id": "3",
     "name": "Gunbarrel 25",
     "description": "25 laps on gunbarrel today",
     "category": "LANDSCAPE",
-    "githubUser": "sSchmidt"
+    "githubUser": "sSchmidt",
+    "created": "2018-04-15T19:09:57.308Z"
   }
 ]
 var tags = [
@@ -39,9 +43,13 @@ var tags = [
 ]
 
 const resolvers = {
+
   Query: {
     totalPhotos: () => photos.length,
-    allPhotos: () => photos
+    allPhotos: (parent, args) => {
+      args.after
+      return photos
+    }
   },
 
   Mutation: {
@@ -49,7 +57,8 @@ const resolvers = {
     postPhoto(parent, args) {
       var newPhoto = {
         id: _id++,
-        ...args.input
+        ...args.input,
+        created: new Date()
       }
       photos.push(newPhoto)
       return newPhoto
@@ -75,7 +84,15 @@ const resolvers = {
         .filter(tag => tag.userID === parent.id)
         .map(tag => tag.photoID)
         .map(photoID => photos.find(p => p.id === photoID))
-  }
+  },
+
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'A valid date time value.',
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
 }
 
 const server = new ApolloServer({
